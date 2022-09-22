c9a484265979ff6aa237a2b099653dbc6c86f71b
Jota <jotacaparros@gmail.com>
2022-09-22 19:24:34 +0200

    22/09/22

--- Prácticas/practicaFactorial.js
+++ Prácticas/practicaFactorial.js
@@ -1,13 +1,32 @@
 let numero;
 let resultado;
+var cadena = '';
 
 function factorial(n){
-    for(let i = 1; i < n; i++){
-     let factorial = n - i;  
-     numero = n;
-     resultado = numero * factorial;
-     document.write(resultado + '<br>');    
-    }
+    // for(let i = 0; i < n; i++){
+    //  let factorial = n - i;
+    //  numero = n;
+    //  resultado = numero * factorial;
+    //  document.write(numero + ' * ' + factorial + ' = ' + resultado + '<br>')
+    //  cadena = factorial + ' * ';
+    //     if(factorial != 0){
+    //         cadena = cadena + ' ' + cadena;
+    //     }
+    //     document.write(cadena);
+    // }
+    // document.write(cadena);
+    let total = 1; 
+	for (let i = 0; i<=n; i++) {
+		total = total * i; 
+       if(i != n - 1 || i != n)
+        cadena += (n - i) + ' *';
+       else
+        cadena += (n - i) + ' = ';
+
+	}
+	// document.write(total);  
+    document.write(cadena); 
+     
 
 }
 
