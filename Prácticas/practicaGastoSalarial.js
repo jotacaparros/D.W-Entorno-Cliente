2ca92c109f4c9ffebd1ebb26970f8cf40c28bbc2
Jota <jotacaparros@gmail.com>
2022-10-06 19:17:57 +0200

    06/10/2022

---
+++ Prácticas/practicaGastoSalarial.js
@@ -0,0 +1,18 @@
+/*Incompleto*/
+/* El departamento de Finanzas de la empresa ACME, S.A. nos ha pedido ayuda para hacer
+el análisis del gasto anual en salarios.Necesitan conocer el total anual empleado en 
+pagar los salarios de los empleados de la empresa y conocer el importe de los tres sueldos más altos.
+
+Deberás solicitar al usuario la introducción de los sueldos anuales de cada empleado (diez en total). 
+Tras ello, habrás de mostrar los sueldos introducidos, la suma total y los tres sueldos más altos 
+ordenados de mayor a menor. */
+
+class empleado{
+   constructor(n,a,s){
+    this.nombre = n;
+    this.apellido = a;
+    this.salario = s;
+   }
+}
+
+Array = new  ()
