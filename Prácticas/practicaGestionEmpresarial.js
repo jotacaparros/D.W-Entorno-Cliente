f0c23f128ef5714241ed513988be7844bb351620
Jota <jotacaparros@gmail.com>
2022-11-07 19:18:07 +0100

    07/11/2022

--- Prácticas/practicaGestionEmpresarial.js
+++ Prácticas/practicaGestionEmpresarial.js
@@ -10,3 +10,44 @@ En caso de ser admitido por cumplir los requisitos, ingresará en la empresa
 y se mostrará un mensaje indicando tal extremo. En caso contrario, se indicará la no admisión,
 el parámetro incumplido (edad o salario) y se solicitará de nuevo una persona al usuario.
 */
+
+let contratados = [];
+class empleado {
+    constructorEmpleado(nombre, edad, salario){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;       
+    }
+
+
+    procesoDeSeleccion(edad, salario){
+
+        if(edad > 60 || salario > 45000){
+            this.noDasLaTalla();
+        }if (contratados.length < 3) {
+            this.contratado();
+        } else {
+            this.noDasLaTalla();
+        }
+
+    }
+
+    contratado() {
+        document.write("Enhorebuena. Has sido seleccionado");
+    }
+
+    noDasLaTalla(){
+        document.write("Lo sentimos mucho, no ha sido seleccionado");
+    }
+
+}
+
+let candidatos = [];
+
+for (let i = 0; i < 3; i++){
+    let candidato = new empleado();
+    candidato
+
+}
+
+procesoDeSeleccion(candidato1.edad, candidato1.salario);
