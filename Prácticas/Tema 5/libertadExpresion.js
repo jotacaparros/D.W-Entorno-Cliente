5d4d9e440b191b176158f57266f8684170e8b37a
jotacaparros <jotacaparros@gmail.com>
2023-01-25 14:43:37 +0100

    25/01/23

--- Prácticas/Tema 5/libertadExpresion.js
+++ Prácticas/Tema 5/libertadExpresion.js
@@ -7,3 +7,24 @@ let expresion2 = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
 let email = "example@gmail.com";
 let resultado2 = expresion2.test(email);
 console.log(resultado);
+
+function escapeHTML(text) {
+    return text.replace(/&/g, "&amp;")
+               .replace(/"/g, "&quot;")
+               .replace(/</g, "&lt;")
+               .replace(/>/g, "&gt;");
+}
+
+var inputText = "<p>This is some text that needs to be escaped.</p>";
+var escapedText = escapeHTML(inputText);
+console.log("Original: " + inputText);
+console.log("Escaped: " + escapedText);
+
+
+let nombre = "John Smith";
+let nombreInvertido = nombre.replace(/(\w+)\s(\w+)/, "$2, $1");
+console.log(nombreInvertido);
+
+let html = "Alguien podría <b>joderte la vida</b> si <script>alert('sabemos donde vives')</script> le dejas <i>hacerlo</i>.";
+let HTMLsinMalicia = html.replace(/<script[\s\S]*?<\/script>/g, "");
+console.log(HTMLsinMalicia);
